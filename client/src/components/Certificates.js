f03e115f5773f01afa9b51a03968dd7a62ca0970
Trinh Gliedt <chuot2008@gmail.com>
2020-12-29 21:21:00 -0800

    v2, added EquityX to Projects and formatted several areas

--- client/src/components/Certificates.js
+++ client/src/components/Certificates.js
@@ -4,16 +4,18 @@ import React from "react";
 import "../styles/CertificatesStyles.css";
 import dojoCert from "../images/cert_Dojo.JPG";
 import cpaCert from "../images/cert_CPA.jpg";
+import awsCert from "../images/cert_AWS.JPG";
 
 function Certificates() {
 
   return (
     <div className="certContainer ml-4 ml-3 mt-2 pl-5 pt-4">
-    <img id="image1" 
-    src={cpaCert} alt="image1"></img>
-    <img id="image2" 
-    src={dojoCert} alt="image2"></img>
-
+    <img className="certImg" id="cpaCert" 
+    src={cpaCert} alt="cpaCert"></img>
+    <img className="certImg" id="dojoCert" 
+    src={dojoCert} alt="dojoCert"></img>
+    <img className="certImg" id="awsCert" 
+    src={awsCert} alt="awsCert"></img>
     </div>
   );
 }
